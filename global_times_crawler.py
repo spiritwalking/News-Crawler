dc347103d484abb0f76776a8886bfabba411737b
troy <961678780@qq.com>
2023-10-09 21:22:18 +0800

    complete gt crawler.py

--- global_times_crawler.py
+++ global_times_crawler.py
@@ -1,64 +1,72 @@
-import xlrd
-import xlwt
-import time
 import logging
-import xlutils.copy
 from time import sleep
 from lxml import etree
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-logging.basicConfig(level=logging.DEBUG)
+
+def create_logger(log_path):
+    """将日志输出到日志文件和控制台"""
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+    # 创建一个handler，用于写入日志文件
+    file_handler = logging.FileHandler(filename=log_path)
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.INFO)
+    logger.addHandler(file_handler)
+
+    return logger
 
 
-class crawler:
-    """ 环球时报新闻爬取类
-
-    类实现了爬取环球时报各个栏目下的一定数量的文章并将其存入excel文件中
-
-    Auther: Gy
-
-    Attributes:
-        url : 爬取的页面的url
-        column_dict : 爬取栏目字典（中文目前未使用）
-        excel_file_name : 保存文档名
-        crawl_page_num : 爬取栏目页面的几页（模拟用户点击MORE）
-        deiver_wait_time : 隐式等待（浏览器加载项目最长等待时间）
-        passage_wait_time : 进入页面后等待多久
-
-    """
-
-    def __init__(self, url, column_dict, crawl_page_num=10, deiver_wait_time=10, page_wait_time=1):
-        """ 初始化 """
+class Crawler:
+    def __init__(self, url, column_list, crawl_page_num=10, deiver_wait_time=10, page_wait_time=1):
+        """初始化函数
+        :param url: 爬取的页面的url
+        :param column_list: 爬取栏目的列表
+        :param crawl_page_num: 爬取栏目页面的几页（模拟用户点击MORE）
+        :param deiver_wait_time: 隐式等待时间（浏览器加载项目最长等待时间）
+        :param page_wait_time: 进入页面后等待多久（防止被封ip）
+        """
         self.url = url
-        self.column_dict = column_dict
+        self.column_list = column_list
         self.crawl_page_num = crawl_page_num
         self.page_wait_time = page_wait_time
         self.browser = webdriver.Chrome()
         self.browser.implicitly_wait(deiver_wait_time)
         self.article_links = []
+        self.logger = create_logger('logs/crawler.log')
 
     def crawl(self):
-        """开始爬取"""
-        logging.info('开始爬取')
-        for column, column_name in self.column_dict.items():
-            logging.info('爬取栏目 {}'.format(column))
-            self.process_a_column(column)
+        """爬虫主程序"""
+        self.logger.info('开始爬取')
+        for column in self.column_list:
+            self.logger.info('爬取栏目: {}...'.format(column))
+            self.process_column(column)
 
-        # try:
-        #     for title, link in self.article_links:
-        #         self.process_a_passage(title, link)
-        # except Exception as e:
-        #     logging.error('处理文章失败，错误信息： {}'.format(e))
-
+        # 将所有文章链接存储到文件中
         with open('links.txt', 'w') as f:
             f.write('\n'.join(self.article_links))
 
-        self.browser.quit()
-        logging.info('爬取完成')
+        # 读取文章链接
+        # with open('links.txt', 'r') as f:
+        #     links_str = f.read()
+        #     links = links_str.split('\n')
+        #
+        # with open('global_times.txt', 'w') as f:
+        #     try:
+        #         for link in links:
+        #             f.write(self.process_passage(link))
+        #     except Exception as e:
+        #         self.logger.error('处理文章失败，错误信息: {}'.format(e))
 
-    def process_a_column(self, column):
-        """爬取一个栏目"""
+        self.browser.quit()
+        self.logger.info('爬取完成')
+
+    def process_column(self, column):
+        """爬取一个栏目中所有文章链接"""
         column_url = self.url + column + '/index.html'
         self.browser.get(column_url)
         sleep(self.page_wait_time)
@@ -68,50 +76,27 @@ class crawler:
             sleep(self.page_wait_time)
 
         source = self.browser.page_source
-        logging.info('开始解析栏目 {}'.format(column))
+        self.logger.info('解析栏目 {}...'.format(column))
         try:
             html_tree = etree.HTML(source)
             passage_list = self.process_column_link(html_tree)
-            self.article_links.extend(passage_list)
+            self.article_links.extend(passage_list)  # 存储文章链接
         except Exception as e:
-            logging.error('解析栏目 {} 链接失败，错误信息： {}'.format(column, e))
-        logging.info('栏目 {} 解析完成'.format(column))
-
-        # try:
-        #     for title, link in passage_list:
-        #         self.process_a_passage(title, link, column)
-        # except Exception as e:
-        #     logging.error('处理文章失败，错误信息： {}'.format(e))
+            self.logger.error('解析栏目{} 失败，错误信息: {}'.format(column, e))
+        self.logger.info('栏目{} 解析完成'.format(column))
 
     def process_column_link(self, html_tree):
-        """处理页面获取所有文章标题和连接
-
-        Args:
-            html : 栏目的树结构
-
-        Returns:
-            一个文章标题和链接元组构成的列表
-        """
+        """处理页面获取所有文章链接"""
         ret_list = []
         passages_list = html_tree.xpath('//div[@class="level01_list"]//div[@class="list_info"]/a')
         if passages_list:
             for passage in passages_list:
-                title = passage.xpath('./text()')[0]
                 link = passage.xpath('./@href')[0]
-                logging.debug('获取到当前文章 {} , url {}'.format(title, link))
                 ret_list.append(link)
         return ret_list
 
-    def process_a_passage(self, title, link):
-        """处理文章信息
-
-        Args:
-            title : 文章标题
-            link : 链接
-            column : 栏目
-        """
-        logging.info('正在处理文章 {} 链接 {}'.format(title, link))
-        row = []
+    def process_passage(self, link):
+        """爬取文章内容"""
         self.browser.get(link)
         sleep(self.page_wait_time)
         source = self.browser.page_source
@@ -121,41 +106,22 @@ class crawler:
             '//div[@class="article_page"]//div[@class="article"]//div[@class="article_content"]//div[@class="article_right"]/br')
         # 找不到文章内容直接退出
         if not content_lst:
-            return
+            self.logger.info('文章爬取失败')
+            return ''
 
         # 获取文章内容
         content = ''
         for one_content in content_lst:
             if one_content.tail:
                 content = content + '\n' + one_content.tail.strip()
-        author_and_publictime_span = html_tree.xpath(
-            '//div[@class="article_page"]//div[@class="article"]//div[@class="article_top"]//div[@class="author_share"]//div[@class="author_share_left"]/span')
-        # 获取作者
-        auther = author_and_publictime_span[0].text.replace('By ', '')
-        # 获取发表时间
-        public_time = author_and_publictime_span[1].text.replace('Published: ', '')
 
-        row.append(title)
-        row.append(auther)
-        row.append(public_time)
-        row.append(link)
-        row.append(content)
-        self.write_excel(row)
-        logging.debug('爬取一条信息 : {} {} {} {} {} {}'.format(title, column, auther, public_time, link, content))
-        logging.info('文章 {} 处理完成'.format(title))
+        print(content)
+        self.logger.info('文章爬取完成')
+        return content
 
 
 if __name__ == '__main__':
-    news_columns_dict = {
-        'sport': '体育',
-        'life': '生活',
-        'world': '世界',
-        'In-depth': '深度',
-        'opinion': '观点',
-        'source': '来源',
-        'china': '中国'
-    }
+    column_list = ['sport', 'life', 'world', 'In-depth', 'opinion', 'source', 'china']
 
-    globaltimes_crawler = crawler('https://www.globaltimes.cn/', news_columns_dict, 2)
+    globaltimes_crawler = Crawler('https://www.globaltimes.cn/', column_list, 10)
     globaltimes_crawler.crawl()
-
